bc9c58da6343ec704cc82f2f25643e0eb68b9d82
harjunpnik <niklasharjunpaa@gmail.com>
2019-08-22 23:46:34 +0300

    Exercise 1.7 Finished

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -26,6 +26,9 @@ const App = () => {
         <p>Good {good}</p>
         <p>Neutral {neutral}</p>
         <p>Bad {bad}</p>
+        <p>All {good + neutral + bad}</p>
+        <p>Average {(good * 1 + neutral * 0 + bad * -1 )/(good + neutral + bad)}</p>
+        <p>Positive {good / (good + neutral + bad) * 100} %</p>
       </div>
     </div>
   )
