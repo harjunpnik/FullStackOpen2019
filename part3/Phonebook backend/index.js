89a35cf7fa43e049cf430abccfe16360bce3d999
harjunpnik <niklasharjunpaa@gmail.com>
2019-08-30 21:40:13 +0300

    Excercise 3.4 Finished

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -37,7 +37,14 @@ app.get('/api/persons/:id', (req, res) => {
     } else{
         res.status(404).end()
     }
-  })
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
 
 app.get('/info', (req, res) => {
     res.send(`<div>
