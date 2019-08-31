f853686823577e86de77854ab73033f1bef8879e
harjunpnik <niklasharjunpaa@gmail.com>
2019-08-31 20:32:51 +0300

    Excercise 3.7 Finished

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(morgan('dev'))
 
 
 let persons = [
