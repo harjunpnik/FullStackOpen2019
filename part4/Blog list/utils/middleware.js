18e57076c51cb266b1a32261a2721e458aff1549
harjunpnik <niklasharjunpaa@gmail.com>
2019-09-15 19:46:39 +0300

    Excercise 4.20* Finished

--- part4/Blog list/utils/middleware.js
+++ part4/Blog list/utils/middleware.js
@@ -3,7 +3,6 @@ const tokenExtractor = (request, response, next) => {
     const authorization = request.get('authorization')
     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
       request.token = authorization.substring(7)
-      
     }
     next()
 }
