bd5af2fd707b33aec84d1e2780cce40ae0c0c42d
harjunpnik <niklasharjunpaa@gmail.com>
2019-09-19 21:26:44 +0300

    Excercise 5.3 Finished

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -7,6 +7,9 @@ function App() {
 
   const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('') 
+  const [author, setAuthor] = useState('') 
+  const [url, setUrl] = useState('') 
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
 
@@ -20,6 +23,7 @@ function App() {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
+      blogService.setToken(user.token)
       setUser(user)
     }
   }, [])
@@ -35,6 +39,7 @@ function App() {
         'loggedBlogappUser', JSON.stringify(user)
       )
 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -49,46 +54,103 @@ function App() {
     window.localStorage.removeItem('loggedBlogappUser')
   }
 
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-        <div>
-          Username: 
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          Password: 
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-      </div>
-    )
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url,
+      likes: 0,
+    }
+
+    console.log(blogObject)
+    blogService
+      .create(blogObject)
+      .then(data => {
+        setBlogs(blogs.concat(data))
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+      })
   }
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        Title: 
+          <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        Author: 
+          <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        Url: 
+          <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>  
+  )
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+      <div>
+        Username: 
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        Password: 
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+  )
 
   return (
     <div>
       <h2>Blogs</h2>
-      <p>{user.name} logged in. 
-      <button onClick={logoutHandler}>
-        logout
-      </button>
-      </p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ?
+        loginForm() :
+          <div>
+            <p> {user.name} logged in. 
+            <button onClick={logoutHandler}>
+              logout
+            </button>
+            </p>
+            {blogForm()}
+            {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+            )}
+          </div>
+      }
     </div>
   )
 }
