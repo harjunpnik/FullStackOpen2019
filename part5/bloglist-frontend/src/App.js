df6955a9bac7d40896ea9b799bbe09a7b6a9bb8a
harjunpnik <niklasharjunpaa@gmail.com>
2019-09-20 20:06:31 +0300

    Excercise 5.8* Finished

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -132,7 +132,7 @@ function App() {
     }
   }
 
-  const renderBlogs = () => blogs.map(blog =>
+  const renderBlogs = () => blogs.sort((a, b) => b.likes - a.likes).map(blog =>
     <Blog 
       key={blog.id} 
       blog={blog}
